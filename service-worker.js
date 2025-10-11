ff4388e580ebc33b0bdbf4856a09261121b77fec
osht <sterling091@gmail.com>
2025-10-11 10:05:54 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='nes-checklist-v9';
-const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./favicon-32.png','./1000007845.jpg','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='nes-checklist-v10';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./1000007845.jpg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
