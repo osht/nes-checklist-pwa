80ecad43c9ce042a7479e4e8be64af37e8c2e8f1
osht <sterling091@gmail.com>
2025-10-11 09:30:31 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,5 @@
-
-const CACHE_NAME = 'nes-checklist-v6';
-const ASSETS = [
-  './favicon-32.png',
-  './',
-  './index.html',
-  './manifest.json',
-  './service-worker.js',
-  './1000007845.jpg',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((cached) => cached || fetch(evt.request))
-  );
-});
+const CACHE_NAME='nes-checklist-v8';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./favicon-32.png','./1000007845.jpg','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
