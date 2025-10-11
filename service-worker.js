0e100b940e5d1fb388ac28e97bbcab95b4245633
osht <sterling091@gmail.com>
2025-10-11 17:55:17 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-// intentionally empty in v26 ES5 build
+const CACHE_NAME='nes-checklist-v23';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./1000007845.jpg','./icons/icon-192.png','./icons/icon-512.png','./favicon-32.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
